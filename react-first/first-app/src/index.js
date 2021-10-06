dd72d5d37d07e8d27c959c7500efd0471bcaa07f
KartikCD <chawdakartik@gmail.com>
2021-10-06 21:17:30 +0530

    deploy done
    Signed-off-by: KartikCD <chawdakartik@gmail.com>

--- react-first/first-app/src/index.js
+++ react-first/first-app/src/index.js
@@ -1,8 +1,30 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function Greeting() {
-  return <h1>Kartik's first component</h1>;
+function BookList() {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return <article>this is a book</article>;
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg"
+      alt="Kartik"
+    />
+  );
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
